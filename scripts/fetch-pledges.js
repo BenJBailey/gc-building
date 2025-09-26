3b9584e672068b14251e2f119f2f76f1493385f4
Ben <benjaminjbailey@gmail.com>
2025-09-26 15:18:51 -0500

    feat(campaign): add script

--- scripts/fetch-pledges.js
+++ scripts/fetch-pledges.js
@@ -1,14 +1,18 @@
 // scripts/fetch.js
 import fs from "fs";
 import fetch from "node-fetch"; // node18+ has fetch built-in, but this works too
+import dotenv from "dotenv"; // if using ESM
+dotenv.config();
 
 const API_URL =
-  "https://api.planningcenteronline.com/giving/v2/pledge_campaigns"; // replace with your API
+  "https://api.planningcenteronline.com/giving/v2/pledge_campaigns/" +
+  process.env.CAMPAIGN_ID;
 
 async function main() {
   const credentials = Buffer.from(
     `${process.env.PLANNING_CENTER_CLIENT_ID}:${process.env.PLANNING_CENTER_CLIENT_SECRET}`
   ).toString("base64");
+  console.log("creds", credentials, process.env.PLANNING_CENTER_CLIENT_ID);
   const res = await fetch(API_URL, {
     headers: {
       Authorization: `Basic ${credentials}`,
